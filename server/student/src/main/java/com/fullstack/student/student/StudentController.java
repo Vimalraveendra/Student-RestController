6496fa936f2d1f0da2cdfac0671f0d3f4fa0de0e
Vimal Raveendran <vimalraveendra@gmail.com>
2024-03-25 23:37:25 +0100

    added StudentRepository & application properties

--- server/student/src/main/java/com/fullstack/student/student/StudentController.java
+++ server/student/src/main/java/com/fullstack/student/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.fullstack.student.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,4 +18,5 @@ public class StudentController {
     public List<Student> getStudents(){
         return  studentServiceImp.getStudents();
     }
+
 }
